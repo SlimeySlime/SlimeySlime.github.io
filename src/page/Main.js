c21f1ebbaef3a37f5af7c0a5611880b6eda60a98
SlimeySlime <35423756+SlimeySlime@users.noreply.github.com>
2022-06-21 17:47:24 +0900

    update start

---
+++ src/page/Main.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+
+const Main = () => {
+
+    return(
+        <div>
+            <h1>home</h1>
+            <p>page</p>
+        </div>
+    )
+}
+
+export default Main
